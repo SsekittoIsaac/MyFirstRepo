dd1cdeac96df0ba671fb109d4c8a8899f8e9f5ef
Ssekitto_Isaac <isaacssekitto18@gmail.com>
2023-06-22 07:53:53 +0000

    container

---
+++ lib/container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Container extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+        // alignment: Alignment.center,
+        // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+        // decoration: BoxDecoration(
+        // borderRadius: BorderRadius.circular(20.0),
+        // ),
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'body.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(HelloWorld());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("CSC DAY CLASS"),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+      body: Body(),
+    ));
   }
 }
